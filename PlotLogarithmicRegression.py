beccd1c1bf32360bce01b15a76cdefefeeb8a34d
Jeongseop Lee <wanderkind@semyung.ac.kr>
2021-11-04 22:31:16 +0900

    Create PlotLogarithmicRegression.py

---
+++ PlotLogarithmicRegression.py
@@ -0,0 +1,66 @@
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+L = []
+
+while True:
+    X = input()
+    
+    if X != '':
+        L.append(float(X))
+        
+    else:
+        break
+
+l = len(L)
+
+x = np.arange(1, l + 1, 1)
+y = np.array(L)
+
+fit = np.polyfit(np.log(x), y, 1)
+print(fit)
+
+a = float(fit[0])
+b = float(fit[1])
+
+def f(n):
+    a*(math.log(n + 1)) + b
+
+def g(n):
+    float(math.log(float(L[n]))) - float(math.log(f(n + 1)))
+
+L0 = []
+Ln = []
+Lp = []
+Lz = []
+for N in range(l - 1):
+    n = N + 1
+    X = L[n] - (a*(math.log(n + 1)) + b)
+    
+    L0.append(X)
+    
+    if X < 0:
+        Ln.append(X)
+    
+    else:
+        Lp.append(X)
+    
+# plt.plot(L0, 'b_')
+# plt.show()
+    
+    N = len(Ln)
+    P = len(Lp)
+
+    print(round(100*N/(N + P), 2), ":", round(100*P/(N + P), 2))
+    
+    Z = -math.log(abs(np.mean(L0)))
+    print(Z)
+    Lz.append(Z)
+
+plt.hist(L0, bins = 80)
+plt.show()
+
+plt.plot(Lz, 'b_')
+plt.show()
