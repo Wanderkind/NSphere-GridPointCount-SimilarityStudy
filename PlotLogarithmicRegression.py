87166ce0a4a43b22c1a521406054eb3d70d9a4c1
Jeongseop Lee <wanderkind@semyung.ac.kr>
2021-11-06 00:45:40 +0900

    Update PlotLogarithmicRegression.py

--- PlotLogarithmicRegression.py
+++ PlotLogarithmicRegression.py
@@ -36,6 +36,7 @@ Ln = []
 Lp = []
 Lpr = []
 Ld = []
+Lm = []
 Lz = []
 for n in range(l):
     X = math.log(L[n]) - math.log(a*(math.log(n + 1)) + b)
@@ -52,30 +53,45 @@ for n in range(l):
     P = len(Lp)
     Nr = 100*N/(N + P)
     Pr = 100*P/(N + P)
-    print("\nN:P ratio :", round(Nr, 2), ":", round(Pr, 2))
+    print('\nN:P ratio :', round(Nr, 2), ':', round(Pr, 2))
     Lpr.append(Pr)
     
     if n != 0:
-        print("StandardDeviation :", stat.stdev(L0))
+        print('StandardDeviation :', stat.stdev(L0))
         Ld.append(stat.stdev(L0))
     
+    print('Mean :', np.mean(L0))
+    Lm.append(np.mean(L0))
     Z = -math.log10(abs(np.mean(L0)))
-    print("MeanSimilarity :", Z)
+    print('MeanSimilarity :', Z)
     Lz.append(Z)
 
-print("\nRegressionLogarithmicCurve :", str(str(a) + "log(x)"), "+", str(b))
+print('\nRegressionLogarithmicCurve :', str(str(a) + 'log(x)'), '+', str(b))
 
+plt.subplot(2, 3, 1)
 plt.plot(L0, 'b_')
-plt.show()
+plt.title('PlotToRegression RatioLog')
 
+plt.subplot(2, 3, 2)
 plt.hist(L0, bins = 100)
-plt.show()
+plt.title('RatioLog Histogram')
 
+plt.subplot(2, 3, 3)
 plt.plot(Lpr, 'g-')
-plt.show()
+plt.title('PositiveLog Ratio')
 
+plt.subplot(2, 3, 4)
 plt.plot(Ld, 'r-')
+plt.title('Log StandardDeviation')
+
+plt.subplot(2, 3, 5)
+plt.plot(Lm, 'm-')
+plt.title('Log Mean')
+
+plt.subplot(2, 3, 6)
+plt.plot(Lz, 'b-')
+plt.title('Mean NegLog')
+
 plt.show()
 
-plt.plot(Lz, 'b-')
-plt.show()
+# fit to be adjusted so that Mean converges to 0.
